5f66a7c2fe4419bbec92433c23785b4f860dafae
Max <74821169+13fragment@users.noreply.github.com>
2024-03-24 01:35:32 +0300

    Add files via upload

---
+++ task_a.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+int main()
+{
+    int length;
+    cin >> length;
+    typedef unordered_set<int> IntSet;
+    IntSet coll;
+    int number = 0;
+    for (int i = 0; i < length; i++) {
+        cin >> number;
+        coll.insert(number);
+
+    }
+    cout << coll.size();
+}
---
+++ task_b.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <string>
+#include <map>
+
+using namespace std;
+int main()
+
+{
+    multimap<int, string> map0;
+    string str;
+    while (getline(cin, str)) {
+        int key = stoi(str.substr(0, str.find(" ")));
+        string value = str.substr(str.find(" ") + 1);
+        map0.insert(make_pair(key, value));
+
+    }
+    map <int, string> ::iterator it = map0.begin();
+    for (int i = 0; it != map0.end(); it++, i++) {
+        cout << it->first << " " << it->second << endl;
+    }
+}
